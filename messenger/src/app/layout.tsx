c1aa4312e1f66d9308a5d08609c04ce8ee65da32
Tim <14timothydeleon@gmail.com>
2024-03-07 16:41:44 -0500

    got google SSO to work

--- messenger/src/app/layout.tsx
+++ messenger/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Providers from '@/components/providers'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><Providers>{children}</Providers></body>
     </html>
   )
 }
