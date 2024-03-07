78609eafa5646bb59ac05701f20ebc4ca2f40c24
Tim <14timothydeleon@gmail.com>
2024-03-07 14:35:12 -0500

    made a button componenet for reusability

---
+++ messenger/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="text-red-500 align ">
+      
+    </div>
+  )
+}
