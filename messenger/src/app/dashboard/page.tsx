75ddd50dc089a95fbc5dc0a82000b99fa02bec6b
Tim <14timothydeleon@gmail.com>
2024-03-07 17:05:31 -0500

    google sso work

--- messenger/src/app/dashboard/page.tsx
+++ messenger/src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import Button from "@/components/ui/Button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 interface pageProps {}
 
 
-const page: FC<pageProps> = ({}) =>{
+const page = async ({}) =>{
+
+    const session = await getServerSession(authOptions)
     return <Button> hello </Button>
 }
 
