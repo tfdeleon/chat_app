aca3a1aa30604b7992d4fa1f0614a0eb17dca50a
tfdeleon <tfdeleon@buffalo.edu>
2024-04-02 16:14:03 -0400

    fixed route.ts for messages

--- messenger/src/app/api/message/send/route.ts
+++ messenger/src/app/api/message/send/route.ts
@@ -1,64 +1,75 @@
-import { fetchRedis } from "@/helpers/redis"
-import { authOptions } from "@/lib/auth"
-import { getServerSession } from "next-auth"
-import { db } from "@/lib/db"
-import { nanoid } from "nanoid"
-import { messageSchema } from "@/lib/validations/message"
-import { pusherServer } from "@/lib/pusher"
-import { toPusherKey } from "@/lib/utils"
+import { fetchRedis } from '@/helpers/redis'
+import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { pusherServer } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
+import { Message, messageSchema } from '@/lib/validations/message'
+import { nanoid } from 'nanoid'
+import { getServerSession } from 'next-auth'
+
 export async function POST(req: Request) {
-    try{
-        const session = await getServerSession(authOptions)
-        const {text, chatId}: {text: string , chatId: string} = await req.json()
+  try {
+    const { text, chatId }: { text: string; chatId: string } = await req.json()
+    const session = await getServerSession(authOptions)
 
-        if(!session) return new Response("Unauthorized", {status: 401})
+    if (!session) return new Response('Unauthorized', { status: 401 })
 
-        const [userId1, userId2] = chatId.split('--')
+    const [userId1, userId2] = chatId.split('--')
 
-        if(session.user.id !== userId1 && session.user.id !== userId2) return new Response('Unauthorized', {status: 401})
-
-        const friendId = session.user.id === userId1 ? userId2 : userId1
-
-        const friendList = await fetchRedis('smembers',`user:${session.user.id}:friends`) as string[]
-        const isFriend = friendList.includes(friendId)
-        if(!isFriend) return new Response("unauthorized", {status: 401})
-
-        const sender = await fetchRedis('get', `user:${session.user.id}`) as string
-        const senderParse = JSON.parse(sender) as User
-
-        const timestamp = Date.now()
-
-        const messageData: Message = {
-            id: nanoid(),
-            senderId: session.user.id,
-            text,
-            timestamp,
-
-        }
-        const message = messageSchema.parse(messageData)
-
-        // Notify all chat subs
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), "incoming-message", message)
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
-            ...message,
-            senderImg: sender.image,
-            senderName: sender.name
-        })
-
-
-
-
-        await db.zadd(`chat:${chatId}:messages`, {
-            score: timestamp,
-            member: JSON.stringify(message),
-        })
-
-        return new Response("ok")
-    }catch(error){
-        if(error instanceof Error){
-            return new Response(error.message, {status: 500})
-        }
-        return new Response("Oops something went wrong!", {status: 500})
-
+    if (session.user.id !== userId1 && session.user.id !== userId2) {
+      return new Response('Unauthorized', { status: 401 })
     }
+
+    const friendId = session.user.id === userId1 ? userId2 : userId1
+
+    const friendList = (await fetchRedis(
+      'smembers',
+      `user:${session.user.id}:friends`
+    )) as string[]
+    const isFriend = friendList.includes(friendId)
+
+    if (!isFriend) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    const rawSender = (await fetchRedis(
+      'get',
+      `user:${session.user.id}`
+    )) as string
+    const sender = JSON.parse(rawSender) as User
+
+    const timestamp = Date.now()
+
+    const messageData: Message = {
+      id: nanoid(),
+      senderId: session.user.id,
+      text,
+      timestamp,
+    }
+
+    const message = messageSchema.parse(messageData)
+
+    // notify all connected chat room clients
+    await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
+
+    await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
+      ...message,
+      senderImg: sender.image,
+      senderName: sender.name
+    })
+
+    // all valid, send the message
+    await db.zadd(`chat:${chatId}:messages`, {
+      score: timestamp,
+      member: JSON.stringify(message),
+    })
+
+    return new Response('OK')
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 })
+    }
+
+    return new Response('Internal Server Error', { status: 500 })
+  }
 }
